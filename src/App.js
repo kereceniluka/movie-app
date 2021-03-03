a3e240eb984670fcbe39aa21f8d6f6943050a0fd
Luka Kerečeni <luka.kereceni@gmail.com>
2021-03-03 13:11:22 +0100

    Moved Modal component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import YouTube from 'react-youtube';
 
 // components
 import Navigation from './components/Navigation';
+import Modal from './components/Modal';
 import Footer from './components/Footer';
 
 // pages
 import Home from './pages/Home';
+import Movies from './pages/Movies';
+import TVShows from './pages/TVShows';
 
 const App = () => {
+
+  const { selectedMovie: { trailer } } = useSelector(state => state.trailerModal);
+
+  const opts = {
+    width: '100%',
+    height: '600px',
+    playerVars: {
+        autoplay: 0,
+    },
+  }
+
   return (
     <>
+      <Modal>
+        {trailer && <YouTube videoId={trailer} opts={opts} />}
+      </Modal>
       <Navigation />
       <main>
         <Route exact path="/" component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/tvshows" component={TVShows} />
       </main>
       <Footer />
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import YouTube from 'react-youtube';
 
 // components
 import Banner from '../components/Banner';
 import Cta from '../components/Cta';
 import PosterSection from '../components/PosterSection';
-import Modal from '../components/Modal';
 
 const Home = () => {
-
-    const { selectedMovie: { trailer } } = useSelector(state => state.trailerModal);
-
-    const opts = {
-        width: '100%',
-        height: '600px',
-        playerVars: {
-            autoplay: 0,
-        },
-    }
-
     return (
         <>
             <Banner />
             <Cta />
             <PosterSection title="Popular movies" type="movie" period="week" />
             <PosterSection title="Popular series" type="tv" period="week" bg="#f2f2f2" />
-            <Modal title="Wonder Woman">
-               {trailer && <YouTube videoId={trailer} opts={opts} />}
-            </Modal>
         </>
     )
 }
