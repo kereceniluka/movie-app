b15a327fb9ac44eda2544dc8efea9e68dacaa97d
Luka Kerečeni <luka.kereceni@gmail.com>
2021-03-03 13:17:08 +0100

    Modified some components & trailerModal action

--- src/components/Cta/index.js
+++ src/components/Cta/index.js
@@ -15,10 +15,10 @@ const Cta = () => {
                     <p style={{ color: '#667375', fontSize: '20px', fontWeight: 400, lineHeight: '28px', marginTop: '15px', marginBottom: '32px' }}>Search for milllions of movies, TV shows and people to discover.</p>
                 </Col>
                 <Col className="d-flex align-items-center justify-content-center" lg={12}>
-                    <LinkContainer to="/signup" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '150px', height: '48px', backgroundColor: '#FCA311', borderColor: '#FCA311', color: '#14213D', fontWeight: 700, margin: '0 10px' }}>
+                    <LinkContainer to="/movies" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '150px', height: '48px', backgroundColor: '#FCA311', borderColor: '#FCA311', color: '#14213D', fontWeight: 700, margin: '0 10px' }}>
                         <Button>Movies</Button>
                     </LinkContainer>
-                    <LinkContainer to="/signup" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '150px', height: '48px', backgroundColor: '#FCA311', borderColor: '#FCA311', color: '#14213D', fontWeight: 700, margin: '0 10px' }}>
+                    <LinkContainer to="/tvshows" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '150px', height: '48px', backgroundColor: '#FCA311', borderColor: '#FCA311', color: '#14213D', fontWeight: 700, margin: '0 10px' }}>
                         <Button>TV shows</Button>
                     </LinkContainer>
                 </Col>
--- src/components/DetailCard/DetailCardStyle.js
+++ src/components/DetailCard/DetailCardStyle.js
@@ -2,13 +2,11 @@ import styled from 'styled-components/macro';
 
 export const StyledOverlay = styled.div `
     position: absolute;
-    top: 0;
-    left: 0;
-    width: 96%;
+    width: 223px;
     height: 300px;
     background: linear-gradient(0, #000000 0%, rgba(30, 29, 29, 0) 100%);
     opacity: 0.7;
     border-radius: 8px;
-    margin-left: 4px;
+    overflow: hidden;
     cursor: pointer;
 `;
