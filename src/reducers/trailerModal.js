1bd8fd2fc5d55e0ac7ddcdfc1411170f42c4db0c
Luka Kerečeni <luka.kereceni@gmail.com>
2021-03-18 15:07:20 +0100

    Modified some reducers

--- src/reducers/trailerModal.js
+++ src/reducers/trailerModal.js
@@ -1,11 +1,16 @@
 import { OPEN_TRAILER_MODAL, CLOSE_TRAILER_MODAL, PLAY_TRAILER_MOVIE } from '../constants/trailerModal';
 
-export const trailerModalReducer = (state = { showModal: false, selectedMovie: {} }, action) => {
+const initialState = {
+    showModal: false,
+    selectedMovie: {},
+}
+
+export const trailerModalReducer = (state = initialState, action) => {
     switch(action.type) {
         case OPEN_TRAILER_MODAL:
-            return { showModal: action.payload, selectedMovie: {} };
+            return { ...state, showModal: action.payload };
         case CLOSE_TRAILER_MODAL:
-            return { showModal: action.payload, selectedMovie: {} };
+            return { ...state, showModal: action.payload };
         case PLAY_TRAILER_MOVIE:
             return { ...state, selectedMovie: action.payload };
         default:
