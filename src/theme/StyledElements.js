234cbb3b1374027c52da5b366b8e1b88cc0eb22b
Luka Kerečeni <luka.kereceni@gmail.com>
2021-03-18 15:13:45 +0100

    Removed AddIcon style

--- src/theme/StyledElements.js
+++ src/theme/StyledElements.js
@@ -41,33 +41,3 @@ export const StyledRow = styled(Row) `
     align-items: center;
     justify-content: center;
 `;
-
-export const AddIcon = styled.div `
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    width: 35px;
-    height: 35px;
-    top: 6%;
-    right: 2%; 
-    transform: translate(-12px, -12px);
-    background: rgba(0, 0, 0, 0.6);
-    border-radius: 4px;
-    cursor: pointer;
-    transition: all .3s ease-in-out;
-
-    &:hover {
-        background: rgba(0, 0, 0, 1);
-    }
-
-    ${sizes.tablet} {
-        top: 5%;
-        right: 5%;
-    }
-
-    ${sizes.desktop} {
-        top: ${({ poster }) => poster && '8%'};
-        right: ${({ poster }) => poster ? '2%' : '4%'};
-    }
-`;
