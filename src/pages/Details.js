cb72b07987b4c04e49c8a2b974dddbdaa074fcb1
Luka Kerečeni <luka.kereceni@gmail.com>
2021-03-19 17:55:41 +0100

    Added ReviewCard component

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyledLargeTitle } from '../theme/StyledElements';
 import { StyledOverlay } from '../components/Banner/BannerStyle'; 
 import { useLocation } from 'react-router-dom';
@@ -8,7 +8,7 @@ import { FaPlay, FaStar } from 'react-icons/fa';
 import Slider from 'react-slick';
 
 // bootstrap
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 
 // actions
 import { fetchDetails } from '../actions/details';
@@ -17,13 +17,16 @@ import { fetchDetails } from '../actions/details';
 import Poster from '../components/Poster';
 import CastCard from '../components/CastCard';
 import Seasons from '../components/Seasons';
+import ReviewCard from '../components/ReviewCard';
 
 const Details = () => {
+
+    const [visibleReviews, setVisibleReviews] = useState(3);
 
     const { state: { id, type } } = useLocation();
 
     const dispatch = useDispatch();
-    const { loading, data: { details, credits } } = useSelector(state => state.details);
+    const { loading, data: { details, credits, reviews } } = useSelector(state => state.details);
 
     useEffect(() => {
         dispatch(fetchDetails(id, type));
@@ -43,8 +46,12 @@ const Details = () => {
         slidesToScroll: 6,
     }
 
+    const handleLoadMore = () => {
+        setVisibleReviews(prevValue => prevValue + 3);
+    }
+
     return (
-        <Container style={{ width: '100vw' }} fluid="true">
+        <Container className="pb-5" style={{ width: '100vw' }} fluid="true">
             {loading === false && (
                 <>
                     <Row style={{ position: 'relative', width: '100%', height: '800px' }}>
@@ -85,6 +92,13 @@ const Details = () => {
                                     <Seasons id={details?.id} numberOfSeasons={details?.number_of_seasons} />
                                 </>
                             )}
+                            <StyledLargeTitle className="mt-5" style={{ marginLeft: '10px' }}>Reviews</StyledLargeTitle>
+                            <Row>
+                                <Col className="d-flex flex-column align-items-center">
+                                    {reviews ? reviews.slice(0, visibleReviews).map((review, index) => <ReviewCard key={index} {...review} />) : <span>There are no reviews...</span>}      
+                                    {visibleReviews <= reviews.length && <Button style={{ backgroundColor: '#FCA311', borderColor: '#FCA311' }} size="md" onClick={handleLoadMore}>Load More</Button>}
+                                </Col>
+                            </Row>
                         </Container>
                     </Row>
                 </>
